77887b4d3796b1754b0eaf1522d33c5ce0055ff5
MertLightz <49613414+MertLightz@users.noreply.github.com>
2020-08-06 14:53:23 +0300

    Update UI.py
    Remove anything to do with excel and prepare for SQL implementation.

--- UI.py
+++ UI.py
@@ -1,9 +1,6 @@
 import sys
-import xlrd
+import mysql.connector
 from PyQt5.QtWidgets import QWidget,QApplication,QTableWidget,QTableWidgetItem,QVBoxLayout
-
-wb = xlrd.open_workbook('stock.xlsx')
-sheet = wb.sheet_by_index(0)
 
 app = QApplication(sys.argv)
 
@@ -14,21 +11,22 @@ qwidget.resize(425,210)
 
 layout = QVBoxLayout()
 
-number_of_rows = sheet.nrows
-
 tableWidget = QTableWidget()
 tableWidget.setColumnCount(3)
-tableWidget.setRowCount(number_of_rows)
+tableWidget.setRowCount(5)
 
 tableWidget.setHorizontalHeaderItem(0,QTableWidgetItem('Name'))
 tableWidget.setHorizontalHeaderItem(1,QTableWidgetItem('Age'))
 tableWidget.setHorizontalHeaderItem(2,QTableWidgetItem('Job'))
 
-for i in range(0, number_of_rows):
-    name = sheet.cell_value(i,0)
-    age = int(sheet.cell_value(i,1))
-    job = sheet.cell_value(i,2)
+database = mysql.connector.connect(
+    host = 'localhost'
+    user = 'yourusername'
+    password = 'yourpassword'
+    )
 
+for i in range(0, 5):
+    name,age,job = 'test','test','test'
     tableWidget.setItem(i,0,QTableWidgetItem(name))
     tableWidget.setItem(i,1,QTableWidgetItem(str(age)))
     tableWidget.setItem(i,2,QTableWidgetItem(job))
