f62f0d7ff106bd9d27c987d90527cbddd1703523
m3rtngs <49613414+m3rtngs@users.noreply.github.com>
2020-09-07 15:16:13 +0100

    Update database.py
    Restart with basic Tkinter.

--- database.py
+++ database.py
@@ -1,66 +1,11 @@
-import sqlite3
 from tkinter import *
-
-# Connect to database
-connector = sqlite3.connect('shop.db')
-
-# Create a cursor
-cursor = connector.cursor()
-
-'''
-# Create a table
-cursor.execute("""CREATE TABLE chocolates (
-        product_name text,
-        stock integer
-    )""")
-    # Datatypes:
-    # NULL - doesn't exist
-    # INTEGER - whole number
-    # REAL - decimal number
-    # TEXT - string
-    # BLOB - stored exactly as it is
-'''
-
-'''
-# Insert one record at a time
-cursor.execute("INSERT INTO chocolates VALUES ('Crunchie', 5)")
-'''
-
-'''
-# Insert multiple records at a time
-multiple_products = [
-                        ('Dairy Milk', 5), 
-                        ('KitKat', 5), 
-                        ('Kinder Bueno', 5)
-                    ]
-cursor.executemany("INSERT INTO chocolates VALUES (?,?)", multiple_products)
-'''
-
-'''
-# Query the database
-cursor.execute("SELECT * FROM chocolates")
-
-    # Ways to fetch:
-    # fetchone() - Last item in the table
-    # fetchmany(2) - Certain number of items
-    # fetchall() - All items
-
-print(cursor.fetchall())
-'''
-
-# Push changes to the database
-connector.commit()
-
-# Close connection
-connector.close()
 
 root = Tk()
 root.title('STOCK CONTROL SYSTEM')
 
-nameLabel = Label(root, text = 'ITEM NAME')
-nameLabel.grid(column = 0, row = 0)
-
-currentStockLabel = Label(root, text = 'STOCK')
-currentStockLabel.grid(column = 2, row = 0)
-
+for i in range(5):
+    for j in range(4):
+        e = Entry(root, width = 20)
+        e.grid(row = i, column = j)
+        
 root.mainloop()
