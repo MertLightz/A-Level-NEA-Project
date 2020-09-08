3a70ddde7e0262498b911b14a0af1d288a33f348
m3rtngs <49613414+m3rtngs@users.noreply.github.com>
2020-09-08 13:25:18 +0100

    Update database.py
    Add apply button.

--- database.py
+++ database.py
@@ -7,5 +7,8 @@ for i in range(5):
     for j in range(4):
         e = Entry(root, width = 20)
         e.grid(row = i, column = j)
+
+applyButton = Button(master = root, text = 'APPLY')
+applyButton.grid(row = i + 1, column = j + 2)
         
 root.mainloop()
